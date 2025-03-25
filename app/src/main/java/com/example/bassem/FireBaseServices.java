f77183c46e784c9c656894481b40e87b2c3a8d89
Bassem <najjarbasem99@gmail.com>
2025-03-25 23:09:46 +0200

    .

---
+++ app/src/main/java/com/example/bassem/FireBaseServices.java
@@ -0,0 +1,52 @@
+package com.example.bassem;
+
+import android.net.Uri;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class FireBaseServices {private static FireBaseServices instance;
+    private FirebaseAuth auth;
+    private FirebaseFirestore fire;
+    private FirebaseStorage storage;
+    private Uri selectedImageURL;
+
+    public Uri getSelectedImageURL() {
+        return selectedImageURL;
+    }
+
+    public void setSelectedImageURL(Uri selectedImageURL) {
+        this.selectedImageURL = selectedImageURL;
+    }
+
+
+    public FireBaseServices()
+    {
+        auth=FirebaseAuth.getInstance();
+        fire=FirebaseFirestore.getInstance();
+        storage=FirebaseStorage.getInstance();
+        selectedImageURL = null;
+    }
+
+    public FirebaseStorage getStorage() {
+        return storage;
+    }
+
+    public FirebaseFirestore getFire() {
+        return fire;
+    }
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+
+    public static FireBaseServices getInstance()
+    {
+        if(instance==null) {
+            instance = new FireBaseServices();
+        }
+        return instance;
+    }
+
+}
